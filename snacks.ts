bfb0fcb2d8733c0ad71ad1e810514b420dc174fc
hermano727 <hh727w@gmail.com>
2025-10-09 14:27:37 -0700

    Add snacks list file

---
+++ snacks.ts
@@ -0,0 +1,9 @@
+const snacks = ["Chocolate", "Snowbunnies", "Gummybears", "Boba", "Chips", "Cookies"]
+
+function printSnacks(snacksL: string[]) {
+    for (let i = 0; i < snacksL.length; i++) {
+        console.log(snacksL[i]);
+    }
+}
+
+printSnacks(snacks);
