33d65131b3fe9c1d8c61f7d39d594871495056d4
Ryan Younan <ryanyounan@gmail.com>
2025-10-09 15:36:03 -0700

    fixed songs function

--- src/music.ts
+++ src/music.ts
@@ -2,7 +2,7 @@ import bold from "./animation";
 
 export const songs = ["Pipe down", "Jumpman", "Nothing on you"];
 
-export function printSongs(): void {
+export default function printSongs(): void {
     songs.forEach((song) => {
         bold(song);
     });
