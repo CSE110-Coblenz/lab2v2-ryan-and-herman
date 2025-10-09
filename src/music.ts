731c8b479289629350befba7d3b073a566f16720
Ryan Younan <ryanyounan@gmail.com>
2025-10-09 15:31:05 -0700

    remove chips

--- src/music.ts
+++ src/music.ts
@@ -2,10 +2,8 @@ import bold from "./animation";
 
 export const songs = ["Pipe down", "Jumpman", "Nothing on you"];
 
-function printSongs(): void {
+export function printSongs(): void {
     songs.forEach((song) => {
         bold(song);
     });
 }
-
-export default {printSongs, songs};
